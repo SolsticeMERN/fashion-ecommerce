7b60e2950bf79e89da6857ea2b2c5bb1e1eb5093
Shakil Sarker <shakilsarker@Shakils-Mac-mini.local>
2026-06-06 02:54:51 +0600

    Updates

---
+++ assets/faq-ILsao2CU.js
@@ -0,0 +1,1 @@
+import"./main-u_Qm-fHq.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelectorAll(".faq-item");t.forEach(e=>{e.querySelector(".faq-question").addEventListener("click",()=>{const s=e.classList.contains("active");t.forEach(c=>c.classList.remove("active")),s||e.classList.add("active")})})});
